fa5c0534f34ff59cc306abec879655a9128e9bd7
PragyaJain006 <projectpragya3@gmail.com>
2024-07-31 00:07:44 +0530

    "Dev-dyanimcs"

---
+++ src/components/Dashboard.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import b from '../utils/sampleData.json';
+import CardComponent from './CardComponent';
+function Dashboard() {
+    const[selectValUser, setSelectValUser]= useState("")
+    const[ selectValDate, setSelectValDate]= useState("")
+    const [cardData, setCardData]= useState("")
+console.log(b)
+  return (
+    <div className='container' style={{display:"flex"}}>
+        <h1>Dashboard</h1>
+        <div className='dropdown'>
+            <div className='user-dropdown'>
+            <p>Users</p>
+        <select value={selectValUser} onChange={(e)=>setSelectValUser(e.target.value)}>
+            {b.data.AuthorWorklog.rows.map((item)=><option key={item.name}>{item.name}</option>)}
+        </select>
+            </div>
+       <div className='date-dropdown'>
+        <p>Time Period</p>
+        <select value={selectValDate} onChange={(e)=>setSelectValDate(e.target.value)}>
+           {b.data.AuthorWorklog.rows[0].dayWiseActivity.map((item)=><option key={item.date}>{item.date}</option>)}
+        </select>
+       </div>
+        </div>
+        {b.data.AuthorWorklog.activityMeta.map((item)=><div className='cards' style={{display:"inline-block"}}>
+        <CardComponent key={item.label} label={item.label} color={item.fillColor}></CardComponent>
+        </div>)}
+    </div>
+  )
+}
+
+export default Dashboard
