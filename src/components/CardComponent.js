8e6d3454400accd67dff4801659bd0faa36b30f2
PragyaJain006 <projectpragya3@gmail.com>
2024-07-31 22:02:49 +0530

    "Data"

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,19 +1,15 @@
 import React from "react";
-// import Card from "@material-ui/core/Card";
-// import CardContent from "@material-ui/core/CardContent";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import CardActions from "@material-ui/core/CardActions";
-import { Card,CardActions,CardContent,Typography,Button } from "@mui/material";
+import { Card,CardActions,CardContent,Typography } from "@mui/material";
 
-export default function CardComponent({label,color}) {
-    
+export default function CardComponent({label,color,data,dayWiseDatas}) {
 	return (
 		<div style={{}}>
 			<Card
 				style={{
-					width: 200,
+					width: "12rem",
 					backgroundColor: color,
+                    padding:"2px",
+                    margin:"5px"
 				}}
 			>
 				<CardContent>
@@ -27,14 +23,21 @@ export default function CardComponent({label,color}) {
 						}}
 						color="textSecondary"
 					>
-						Keep Motivated
+						{data && data?.map((item)=>{
+                            if(item?.name===label){
+                                return <h2 key={item.value}>{item.value}</h2>
+                            }
+                        })}
+						{dayWiseDatas && dayWiseDatas?.map((item)=>{
+							if(item?.label===label){
+							return <h2 key={item.label}>{item.count}</h2>
+							}
+						})}
 					</Typography>
-					<Typography variant="body2" component="p">
-						Stay Happy
-					</Typography>
+					
 				</CardContent>
 				<CardActions>
-					<Button size="small">Stay Safe.....</Button>
+					
 				</CardActions>
 			</Card>
 		</div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,128 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import b from '../utils/sampleData.json';
 import CardComponent from './CardComponent';
 function Dashboard() {
     const[selectValUser, setSelectValUser]= useState("")
     const[ selectValDate, setSelectValDate]= useState("")
-    const [cardData, setCardData]= useState("")
-console.log(b)
+    const [cardData, setCardData]= useState([])
+    const[dayWiseData, setDayWiseData]=useState([])
+let arr=[]
+let total=[]
+let res={}
+let dayWise=[]
+let totaldayWise=[]
+let resDay={}
+
+useEffect(()=>{
+   // debugger;
+if(selectValUser!=="" && selectValUser!=="All"){
+    b.data.AuthorWorklog.rows.map((item)=>{
+        if(!selectValDate || selectValDate===""){
+            item.totalActivity.filter((ele)=>{
+                if(selectValUser===item.name){
+                    arr.push(ele)
+                    setCardData(arr)
+                }
+            })
+            setDayWiseData([])
+        }else{
+            if(item.name===selectValUser){
+                item.dayWiseActivity.filter((ele)=>{
+                    if(ele.date===selectValDate){
+                       ele.items.children.map((i)=>dayWise.push(i))
+                    }
+                    
+                 })
+            }
+           setDayWiseData(dayWise)
+            setCardData([])
+        }
+       
+    })
+}else if(selectValUser==="All"){
+    if(!selectValDate || selectValDate===""){
+        b.data.AuthorWorklog.rows.map((item)=>{
+            item.totalActivity.map((ele)=>{
+             total.push(ele)
+            })
+           })
+           total.forEach(obj => {
+            const key = obj.name;
+            const value = parseInt(obj.value, 10);
+            if (!res[key]) {
+                res[key] = 0;
+            }
+            res[key] += value;
+        });
+        const resultArray = Object.entries(res).map(([name, value]) => ({
+            name,
+            value
+        }));
+        setDayWiseData([])
+        setCardData(resultArray)
+    }else{
+        b.data.AuthorWorklog.rows.map((item)=>{
+            item.dayWiseActivity.filter((ele)=>{
+                if(selectValDate===ele.date){
+                    ele.items.children.map((i)=>{
+                        totaldayWise.push(i)
+                    })
+                }
+            })
+                
+        })
+        totaldayWise.forEach(obj=>{
+         
+            const key=obj.label;
+            const value=parseInt(obj.count,10);
+            if(!resDay[key]){
+                resDay[key]=0;
+            }
+            resDay[key]+=value
+            console.log(resDay,"res")
+        })
+       
+        const resultArray = Object.entries(resDay).map(([name, value]) => ({
+            name,
+            value
+        }));
+       setCardData(resultArray);
+       setDayWiseData([])
+    }
+ 
+
+}
+else{
+    setCardData([])
+}
+
+},[selectValUser,selectValDate])
+
   return (
-    <div className='container' style={{display:"flex"}}>
+    <div className='container'>
         <h1>Dashboard</h1>
         <div className='dropdown'>
             <div className='user-dropdown'>
             <p>Users</p>
         <select value={selectValUser} onChange={(e)=>setSelectValUser(e.target.value)}>
-            {b.data.AuthorWorklog.rows.map((item)=><option key={item.name}>{item.name}</option>)}
+        <option value="">Select User</option>
+        <option value="All">All Users</option>
+            {b.data.AuthorWorklog.rows.map((item)=> <option key={item.name}>{item.name}</option>
+
+)}
+            
         </select>
             </div>
        <div className='date-dropdown'>
         <p>Time Period</p>
         <select value={selectValDate} onChange={(e)=>setSelectValDate(e.target.value)}>
+        <option value="">Select Date</option>
            {b.data.AuthorWorklog.rows[0].dayWiseActivity.map((item)=><option key={item.date}>{item.date}</option>)}
         </select>
        </div>
         </div>
         {b.data.AuthorWorklog.activityMeta.map((item)=><div className='cards' style={{display:"inline-block"}}>
-        <CardComponent key={item.label} label={item.label} color={item.fillColor}></CardComponent>
+        <CardComponent key={item.label} label={item.label} color={item.fillColor} data={cardData} dayWiseDatas={dayWiseData}></CardComponent>
         </div>)}
     </div>
   )
